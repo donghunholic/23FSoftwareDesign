478861c0ee4d18ec35c5e4bb3f2f9e5b9556db33
minsusun <poodding397@gmail.com>
2023-11-23 15:13:30 +0900

    fix: Typo about calling convertHTML()

--- src/main/java/com/mdeditor/sd/Block.java
+++ src/main/java/com/mdeditor/sd/Block.java
@@ -41,7 +41,7 @@ public class Block extends JTextPane {
      */
     public void renderHTML(){
         this.setMdText(getCurText());
-        this.setText(Utils.stringToHtml(getMdText()));
+        this.setText(Utils.convertHTML(getMdText()));
 
     }
 
