870f5824de618d26ddd856df5723a8a70fcb1014
KYH <kyh102824@postech.ac.kr>
2023-11-23 11:52:42 +0900

    #12 feat: Block event handling update

--- src/main/java/com/mdeditor/sd/BlockManager.java
+++ src/main/java/com/mdeditor/sd/BlockManager.java
@@ -4,37 +4,36 @@ import com.intellij.util.containers.LinkedListWithSum;
 import org.apache.batik.bridge.Mark;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class BlockManager {
-    private final List<Block> blockList;
+    private final LinkedList<Block> blockList;
     private final MarkdownEditor mdEditor;
 
     public BlockManager(MarkdownEditor mdE) {
         this.blockList = new LinkedList<>();
         this.mdEditor = mdE;
     }
-    /* add block to blockList */
-    public boolean attach(Block block) {
-        return blockList.add(block);
-    }
 
-    /* remove block to blockList */
-    public boolean detach(Block block) {
-        return blockList.remove(block);
-    }
-
+    /**
+     * Handle Focus event
+     * Block is created or deleted, request update to MarkdownEditor
+     */
     public void update(Block block, BlockEvent e) {
         switch (e) {
             case NEW_BLOCK -> {
                 blockList.add(blockList.indexOf(block), new Block(this));
+
             }
             case DELETE_BLOCK -> {
                 blockList.remove(block);
             }
             case OUTFOCUS_BLOCK_UP -> {
+                int temp = blockList.indexOf(block);
+                blockList.get(temp == 0 ? 0 : temp - 1).requestFocus();
             }
             case OUTFOCUS_BLOCK_DOWN -> {
+                int temp = blockList.indexOf(block);
+                blockList.get(temp == blockList.size() - 1 ? temp : temp + 1).requestFocus();
             }
             default -> {
             }
