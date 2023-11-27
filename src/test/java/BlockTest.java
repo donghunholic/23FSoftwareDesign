2531af2d876687c2279ecaaa86c063c60985b8d2
minsusun <poodding397@gmail.com>
2023-11-27 23:14:29 +0900

    test: add block test with Hyeil Jeong(@hyelie) add mockito library

---
+++ src/test/java/BlockTest.java
@@ -0,0 +1,59 @@
+import com.mdeditor.sd.Block;
+import com.mdeditor.sd.BlockEvent;
+import com.mdeditor.sd.BlockManager;
+import com.mdeditor.sd.Utils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BlockTest {
+    @Mock
+    BlockManager manager;
+    Block block;
+
+    @BeforeEach
+    void clearBlock() {
+        block = new Block(manager);
+    }
+
+    @Test
+    void testBlockMdTextGetterSetter() {
+        String content = "# Head1";
+        block.setMdText(content);
+        assertEquals(block.getMdText(), content);
+    }
+
+    @Test
+    void testBlockRenderHTML() {
+        String content = "# Head1";
+        block.setText(content);
+        block.renderHTML();
+        assertEquals(block.getMdText(), content);
+        assertEquals(block.getText(), Utils.stringToHtml(content));
+    }
+
+    @Test
+    void testRenderMD() {
+        String content = "# Head1";
+        block.setMdText(content);
+        block.renderMD();
+        assertEquals(block.getText(), content);
+    }
+
+    @Test
+    void testGetBlock() {
+        assertEquals(block.getBlock(), block);
+    }
+
+    @Test
+    void testGetManager() {
+        assertEquals(block.getManager(), manager);
+    }
+
+    @Test
+    void testRequestFocusInWindow() {
+        assertFalse(block.requestFocusInWindow());
+    }
+}
