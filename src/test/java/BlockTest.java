4f03b0f7d330b0c87d209e1305a51c10765fa15e
minsusun <poodding397@gmail.com>
2023-12-10 12:01:39 +0900

    #36 test: fix blockTest with Hawoo Jung(@howru0321)

--- src/test/java/BlockTest.java
+++ src/test/java/BlockTest.java
@@ -2,6 +2,7 @@ import com.mdeditor.sd.Block;
 import com.mdeditor.sd.BlockEvent;
 import com.mdeditor.sd.BlockManager;
 import com.mdeditor.sd.Utils;
+import org.jsoup.Jsoup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -25,23 +26,29 @@ public class BlockTest {
         assertEquals(block.getMdText(), content);
     }
 
-//    @Test
-//    void testBlockRenderHTML() {
-//        String content = "# Head1";
-//        block.setMdText(content);
-//        block.renderHTML();
-//        // assertEquals(block.getContentType(), "text/html");
-//        assertEquals(block.getMdText(), content);
-//        assertEquals(block.getText(), Utils.stringToHtml(content));
-//    }
+    @Test
+    void testBlockRenderHTML() {
+        String content = "# Head1";
+        block.setMdText(content);
+        block.renderHTML();
+        assertEquals(block.getContentType(), "text/html");
+        assertEquals(block.getMdText(), content);
+        // check css in head
+        assertFalse(Jsoup.parse(block.getText()).head().select("style").isEmpty());
+        // check body
+        String test = Jsoup.parse(block.getText()).select("body > *").get(0).outerHtml();
+        assertEquals(test, "<h1> Head1 </h1>");
+    }
 
     @Test
     void testRenderMD() {
         String content = "# Head1";
         block.setMdText(content);
+        block.setContentType("text/html");
         block.renderMD();
-        // assertEquals(block.getContentType(), "text/plain");
+        assertEquals(block.getContentType(), "text/plain");
         assertEquals(block.getMdText(), content);
+        assertEquals(block.getText(), content);
     }
 
     @Test
@@ -57,5 +64,20 @@ public class BlockTest {
     @Test
     void testRequestFocusInWindow() {
         assertFalse(block.requestFocusInWindow());
+    }
+
+    @Test
+    void testDestruct() {
+        String content = "# Head1";
+        block.setMdText(content);
+        block.destruct();
+        assertNull(block.getMdText());
+        assertNull(block.getManager());
+    }
+
+    @Test
+    void testIndent() {
+        block.setIndent_level(10);
+        assertEquals(block.getIndent_level(), 10);
     }
 }
