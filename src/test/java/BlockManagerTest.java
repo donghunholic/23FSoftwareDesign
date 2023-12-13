bf6a12454d8dd5e071b107f7011ce5ed115cb8d4
minsusun <poodding397@gmail.com>
2023-12-14 01:54:16 +0900

    #36 test: enhance BlockManagerTest with Hawoo Jung(@howru0321) BlockManager 100%/100%/96%

--- src/test/java/BlockManagerTest.java
+++ src/test/java/BlockManagerTest.java
@@ -1,18 +1,132 @@
-import com.mdeditor.sd.Block;
-import com.mdeditor.sd.BlockManager;
+import com.mdeditor.sd.*;
 import com.mdeditor.sd.editor.MarkdownEditor;
 import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class BlockManagerTest {
-    @Mock
     MarkdownEditor markdownEditor;
-    BlockManager blockManager = new BlockManager(markdownEditor);
+    BlockManager blockManager;
+
+    @BeforeEach
+    void setupBlockManagerTest() {
+        markdownEditor = mock(MarkdownEditor.class);
+        blockManager = new BlockManager(markdownEditor);
+    }
+
+    void setupUpdateTest() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+                """;
+        blockManager.setBlocks(md);
+    }
+
+    @Test
+    void testUpdateUPDATE_BLOCK() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.UPDATE_BLOCK, 10));
+    }
+
+    @Test
+    void testUpdateNEW_BLOCK() {
+        setupUpdateTest();
+        int prev = blockManager.getBlockList().size();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.NEW_BLOCK, 10));
+        assertEquals(blockManager.getBlockList().size(), prev + 1);
+    }
+
+    @Test
+    void testUpdateDELTE_BLOCK() {
+        setupUpdateTest();
+        int prev = blockManager.getBlockList().size();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.DELETE_BLOCK, 10));
+        assertEquals(blockManager.getBlockList().size(), prev - 1);
+    }
+
+    @Test
+    void testUpdateOUTFOCUS_BLOCK_UP() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.OUTFOCUS_BLOCK_UP, 10));
+    }
+
+    @Test
+    void testUpdateOUTFOCUS_BLOCK_DOWN() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.OUTFOCUS_BLOCK_DOWN, 10));
+    }
+
+    @Test
+    void testUpdateOUTFOCUS_CLICKED() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.OUTFOCUS_CLICKED, 10));
+    }
+
+    @Test
+    void testUpdateTRANSFORM_MULTI() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.TRANSFORM_MULTI, 10));
+        assertTrue(blockManager.getBlockList().get(2) instanceof MultiLineBlock);
+    }
+
+    @Test
+    void testUpdateTRANSFORM_SINGLE() {
+        setupUpdateTest();
+        assertDoesNotThrow(() -> blockManager.update(blockManager.getBlockList().get(2), BlockEvent.TRANSFORM_SINGLE, 10));
+        assertTrue(blockManager.getBlockList().get(2) instanceof SingleLineBlock);
+    }
+
+    @Test
+    void testBlockParse() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+> Cuote cuot
+                """;
+        blockManager.setBlocks(md);
+        assertDoesNotThrow(() -> blockManager.blockParse(blockManager.getBlockList().size() - 1));
+        assertEquals(blockManager.getBlockList().size(), 5);
+    }
+
+    @Test
+    void testBlockParseExtreme() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+> Cuote cuot
+                """;
+        blockManager.setBlocks(md);
+        Block b = blockManager.getBlockList().get(blockManager.getBlockList().size() - 1);
+        b.setMdText(b.getMdText() + "\n# Head 1\n1. spy\n> maybe");
+        assertDoesNotThrow(() -> blockManager.blockParse(blockManager.getBlockList().size() - 1));
+        assertEquals(blockManager.getBlockList().size(), 8);
+    }
 
     @Test
     void testParseStringIntoBlocks() {
@@ -62,5 +176,129 @@ and one more line""";
         assertNotNull(result2);
         assertEquals(-1, result2.getLeft());  // Expecting -1 as no TABLE block is present
         assertEquals(-1, result2.getRight()); // Expecting -1 as no TABLE block is present
+    }
+
+    @Test
+    void testSetBlocks() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+                """;
+        assertDoesNotThrow(() -> blockManager.setBlocks(md));
+    }
+
+    @Test
+    void testSetBlocksEmptyMD() {
+        blockManager.setBlocks("");
+        assertTrue(blockManager.getBlockList().get(0).getMdText().isEmpty());
+    }
+
+    @Test
+    void testGetBlockList() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+                """;
+        blockManager.setBlocks(md);
+        assertEquals(blockManager.getBlockList().size(), 5);
+    }
+
+    @Test
+    void testExtractFullMd() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot""";
+        String result = """
+# Head 1
+
+## Head 2
+
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+
+""";
+        blockManager.setBlocks(md);
+        assertEquals(blockManager.extractFullMd(), result);
+    }
+
+    @ParameterizedTest(name = "testRenderAll_{index}")
+    @CsvSource({"-1", "10", "150"})
+    void testRenderAll(int pos) {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+                """;
+        blockManager.setBlocks(md);
+        assertDoesNotThrow(() -> blockManager.renderAll(pos));
+    }
+
+    @Test
+    void testRenderAllBoundary() {
+        String md = """
+# Head 1
+## Head 2
+### Head 3
+
+- UOL
+- UOL
+
+> Quote quot
+                """;
+        blockManager.setBlocks(md);
+        assertDoesNotThrow(() -> blockManager.renderAll(-1));
+    }
+
+    @Test
+    void testMergeBlock() {
+        String md = """
+> Quote
+- whut
+> cuote
+                """;
+        blockManager.setBlocks(md);
+        blockManager.getBlockList().get(1).setMdText("> whut");
+        assertDoesNotThrow(() -> blockManager.mergeBlock(1));
+        assertEquals(blockManager.getBlockList().size(), 1);
+    }
+
+    @Test
+    void testManageBlock() {
+        String md = """
+> Quote
+- whut
+> cuote
+                """;
+        blockManager.setBlocks(md);
+        blockManager.getBlockList().get(1).setMdText("> whut");
+        assertDoesNotThrow(() -> blockManager.manageBlock(1));
+        assertEquals(blockManager.getBlockList().size(), 1);
     }
 }
