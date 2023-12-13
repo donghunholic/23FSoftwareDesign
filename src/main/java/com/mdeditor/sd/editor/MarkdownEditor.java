d7462fa8c8d7f903f6eed5c23d07caea5bf4fbed
Donghun Shin <sdh728@postech.ac.kr>
2023-12-14 04:04:32 +0900

    hotfix: revert parseBlock Function before refactoring

--- src/main/java/com/mdeditor/sd/editor/MarkdownEditor.java
+++
@@ -1,7 +0,0 @@
-package com.mdeditor.sd.editor;
-
-import com.intellij.openapi.fileEditor.FileEditor;
-
-public interface MarkdownEditor extends FileEditor {
-    void updateUI();
-}
--- src/main/java/com/mdeditor/sd/manager/BlockEvent.java
+++
@@ -1,9 +0,0 @@
-package com.mdeditor.sd.manager;
-
-/**
- * Type of block event occured in our editor.
- */
-public enum BlockEvent {
-    NEW_BLOCK, DELETE_BLOCK, OUTFOCUS_BLOCK_UP, OUTFOCUS_BLOCK_DOWN, OUTFOCUS_CLICKED,
-    TRANSFORM_MULTI, TRANSFORM_SINGLE, UPDATE_BLOCK;
-}
